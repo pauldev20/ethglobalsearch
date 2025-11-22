4b72cb9d64d6615e023d3ee246ecf7ecdd2b3666
Patrick Fuchs <email@patrick-fuchs.com>
2025-11-22 16:02:13 -0300

    upadte only links

--- backend/server/services/scheduler.py
+++ backend/server/services/scheduler.py
@@ -6,8 +6,8 @@ import os
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from openai import AsyncOpenAI
-from ._download import download_projects, download_hackathons
-from ._fill_db import fill_db
+from ._download import download_links, download_projects, download_hackathons
+from ._fill_db import fill_db, fill_db_links
 from ._fill_search import fill_search
 from ._fill_similarity import fill_similarity
 
@@ -17,32 +17,46 @@ scheduler = AsyncIOScheduler()
 async def update_projects(db: psycopg2.extensions.connection,
                           es: elasticsearch.Elasticsearch,
                           openai_client: AsyncOpenAI):
-    # hackathons = await download_hackathons()
-    # additional = ["trifecta-tee", "trifecta-zk", "trifecta-agents"]
-    # hackathons.extend([{"slug": h} for h in additional])
-    # for h in hackathons:
-    #     print(h)
-    #     projects = await download_projects(h["slug"])
-    #     print(f"Downloaded {len(projects)} projects for hackathon {h['slug']}")
-    #     if len(projects) > 0:
-    #         count = fill_db(db, projects)
-    #         print(f"Successfully loaded {count} projects into the database!")
-    # print("Filling search", flush=True)
-    # count = await fill_search(db, es, openai_client)
-    # print(f"Successfully loaded {count} projects into Elasticsearch!")
+    hackathons = await download_hackathons()
+    additional = ["trifecta-tee", "trifecta-zk", "trifecta-agents"]
+    hackathons.extend([{"slug": h} for h in additional])
+    for h in hackathons:
+        print(h)
+        projects = await download_projects(h["slug"])
+        print(f"Downloaded {len(projects)} projects for hackathon {h['slug']}")
+        if len(projects) > 0:
+            count = fill_db(db, projects)
+            print(f"Successfully loaded {count} projects into the database!")
+    print("Filling search", flush=True)
+    count = await fill_search(db, es, openai_client)
+    print(f"Successfully loaded {count} projects into Elasticsearch!")
     print("Filling similarity", flush=True)
     similarity_count = await fill_similarity(db, es)
     print(f"Successfully loaded {similarity_count} similarities into the database!")
 
 
+async def update_links(db: psycopg2.extensions.connection,
+                       es: elasticsearch.Elasticsearch):
+    print("Updating links", flush=True)
+    links = await download_links()
+    print(f"Downloaded {len(links)} links")
+    fill_db_links(db, links)
+    print(f"Successfully loaded {len(links)} links into the database!")
+
+
 async def start_scheduler(db: psycopg2.extensions.connection,
                           es: elasticsearch.Elasticsearch,
                           openai_client: AsyncOpenAI):
-    if os.getenv("NO_UPDATE", "false") == "true":
-        return
-    asyncio.create_task(update_projects(db, es, openai_client))
-    # scheduler.add_job(update_projects,
-    #                   "interval",
-    #                   minutes=50,
-    #                   args=[db, es, openai_client])
-    # scheduler.start()
+    if os.getenv("NO_UPDATE", "false") == "false":
+        asyncio.create_task(update_projects(db, es, openai_client))
+        scheduler.add_job(update_projects,
+                        "interval",
+                        minutes=50,
+                        args=[db, es, openai_client])
+    else:
+        asyncio.create_task(update_links(db, es))
+        scheduler.add_job(update_links,
+                        "interval",
+                        minutes=50,
+                        args=[db, es])
+    scheduler.start()
