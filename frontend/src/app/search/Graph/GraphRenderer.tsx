1b870ecd1d83a736c20246bd2c55b2aa0534d413
Paul Geeser <commit@pauldev.sh>
2025-11-23 03:37:31 -0300

    Formatted

--- frontend/src/app/search/Graph/GraphRenderer.tsx
+++ frontend/src/app/search/Graph/GraphRenderer.tsx
@@ -1,18 +1,23 @@
-import { getGraph, GraphData } from "@/lib/api";
+import { type GraphData, getGraph } from "@/lib/api";
 import GraphComponent from "./GraphComponent";
 
-export default async function GraphRenderer({ query, events, types, organizations }: { query: string, events: string, types: string, organizations: string }) {
-	const graphData: GraphData = await getGraph(query, events, types, organizations, 0.85);
+export default async function GraphRenderer({
+    query,
+    events,
+    types,
+    organizations,
+}: { query: string; events: string; types: string; organizations: string }) {
+    const graphData: GraphData = await getGraph(query, events, types, organizations, 0.85);
 
-	return (
-		<div className="w-full max-h-full h-full flex items-center justify-center relative overflow-hidden">
-			{graphData.nodes.length > 0 ? (
-				<GraphComponent graphData={graphData} />
-			) : (
-				<div className="flex items-center justify-center h-full text-muted-foreground">
-					Loading graph data...
-				</div>
-			)}
-		</div>
-	);
+    return (
+        <div className="w-full max-h-full h-full flex items-center justify-center relative overflow-hidden">
+            {graphData.nodes.length > 0 ? (
+                <GraphComponent graphData={graphData} />
+            ) : (
+                <div className="flex items-center justify-center h-full text-muted-foreground">
+                    Loading graph data...
+                </div>
+            )}
+        </div>
+    );
 }
