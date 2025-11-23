78a8ee81cc7879a24bb4959066a81d598f69d7d4
Claus-Peter Käpplinger <5921903+Clausinho@users.noreply.github.com>
2025-11-23 06:42:41 +0000

    config: allow server actions from codespaces origins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,12 +4,21 @@ const nextConfig: NextConfig = {
 	output: "standalone",
 	images: {
 		remotePatterns: [
-		  {
-			protocol: 'https',
-			hostname: '*.cloudflarestorage.com',
-		  },
+			{
+				protocol: 'https',
+				hostname: '*.cloudflarestorage.com',
+			},
 		],
 	},
+	experimental: {
+		serverActions: {
+			allowedOrigins: [
+				'localhost:3000',
+				'supreme-adventure-9g54p6pv93pqwq-3000.app.github.dev',
+				'https://ethsearch.pauldev.sh/'
+			]
+		}
+	}
 };
 
 export default nextConfig;
